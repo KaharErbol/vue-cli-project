ebf9b2a1a92045c5648fb48c0ad16cd545114657
KaharErbol <kaharerbol@gmail.com>
2024-06-08 11:36:32 -0700

    created dynamic backdrop

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input placeholder="email" type="text" ref='name'>
+  <button @click="toggleModal">Subscribe</button>
+  <div v-if="showModal">
+    <Modal :header="header" :moto="moto" theme="sale" @close="toggleModal"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'Beetlia',
+      header: 'Sign up for consultation!',
+      moto: 'Sunny',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      // console.log(this.$refs.name)
+      // this.$refs.name.classList.add('active')
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
@@ -22,5 +40,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  color: rgb(21, 123, 170);
 }
 </style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale: theme === 'sale'}">
+      <h1>{{ moto }}</h1>
+      <h3>Now the dark days are done</h3>
+      <h3>And the bright days are here</h3>
+      <p>{{ header }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'moto', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  h1, h3 {
+    color: rgb(208, 147, 6);
+  }
+
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+
+  .modal.sale h1{
+    color: white;
+  }
+</style>
